34d313df64c352d4fa0c6721df68d94cafc3ca8d
kubix97 <51679705+kubix97@users.noreply.github.com>
2020-12-20 22:48:22 +0100

    Added code for creating codebook

--- detect_face.py
+++ detect_face.py
@@ -1,5 +1,10 @@
 import face_extractor
+import Codebook as cb
 
-extractor = face_extractor.FaceExtractor('faces', 'cropped_images')
+#extractor = face_extractor.FaceExtractor('faces', 'cropped_images')
+#extractor.preprocess_images()
 
-extractor.preprocess_images()
+codebook = cb.Codebook()
+#codebook.create_codebook()
+v_angle, h_angle = codebook.Estimate_angles_for_img('cropped_images_copy/Person15/0person15115-30-75.jpg', 'Codebook_cell8x8')
+print("Estimated orientation: Vertical= {}, Horizontal= {}".format(v_angle, h_angle))
